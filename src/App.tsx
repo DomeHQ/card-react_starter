e00e7707612bf0e6132ca70b9e1a807c253a4ebc
SarveshInTouch <sarveshpatil@intouchapp.com>
2026-01-28 13:10:31 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react'
+import { CardSdk, getKeyFromBlob, type CardEventHandler } from 'dome-embedded-app-sdk';
+import './App.css'
+
+function App() {
+   // User object state
+   const [user, setUser] = useState<any>(null);
+   // store the SDK state to access it later
+   const [sdk, setSdk] = useState<any>(null);
+   const [initError, setInitError] = useState<any>(null);
+
+   useEffect(() => {
+     // decryption blob for the card shared with devs goes here
+     const reactStarterDecBlob = {v: 0, seed: 0, obf: []};
+
+     // Handle dome card events
+     const eventHandler: CardEventHandler = {
+      // here you will recieve the init data with user info, theme, permissions etc.
+       onInit: (data: any) => {
+         setUser(data?.user);
+       },
+       // onInitError will return an error object with message and error_code if initialization failsx
+       onInitError: (data: any) => {
+        setInitError(data);
+       },
+       onError: (data: { message: string; error_code: string | number; }) => {
+         console.error("Some Error", `${data.message} (${data.error_code})`);
+       },
+     };
+
+     // Initialize card with secret code and event handler
+     CardSdk.init(getKeyFromBlob(reactStarterDecBlob), eventHandler)
+       .then((sdk) => {
+        setSdk(sdk);
+        console.debug("React Starter initialized");
+       })
+       .catch((err) => {
+         console.error("Init failed", err);
+       });
+   }, []);
+
+   return (
+     <div className="main">
+       {user ? (
+         <>
+           <h1>
+             Hello, {user.name?.given} {user.name?.family}
+           </h1>
+           <p>Congratulations! Your card is running. 🎉</p>
+         </>
+       ) : initError ? (
+        <>
+         <h3>Initialization Failed</h3>
+         <p>{initError.message} {initError.error_code}</p>
+        </>
+       ) : (
+          <p>Loading...</p>
+       )}
+     </div>
+   );
+}
+
+export default App
